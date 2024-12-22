5ac489fcc73801962fa557e793fb09c581b71c1e
Kshitij <kshitijraj001@gmail.com>
2024-12-22 10:30:57 +0530

    adding my java project

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a=56;
+        System.out.println(a);
+        System.out.println("Hello world!");
+    }
+}
